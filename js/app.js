1f47b2165abcc02e647612fb16a2ff54ce0399ac
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-16 18:33:47 -0300

    se crea la nueva función para ver más texto

--- js/app.js
+++ js/app.js
@@ -15,3 +15,20 @@ const cambiarTitulo = () => {
   h1.innerText = 'Nuevo título'
   h1.className = 'display-1 text-warning'
 }
+
+const verMas = () => {
+  console.log('Desde la función ver más');
+  //  OPCIÓN 1
+  //  Crear un párrafo nuevo
+  const parrafoNuevo = document.createElement('p')
+  console.log(parrafoNuevo);
+  parrafoNuevo.innerText = 'También puedes añadir oyentes de eventos para responder a interacciones del usuario, como clics o pulsaciones de teclas. Es crucial tener en cuenta el rendimiento al manipular el DOM, ya que operaciones frecuentes pueden impactar en el rendimiento de la página. En resumen, el DOM proporciona una interfaz poderosa para manipular la estructura y el contenido de una página web, lo que permite crear interacciones dinámicas y experiencias personalizadas para los usuarios.'
+  console.log(parrafoNuevo);
+  
+  //  Agragar el párrafo en el HTML
+}
+
+const btnVerMas = document.getElementById('btn-ver-mas')
+
+//  Agregar un manejador de eventos
+btnVerMas.addEventListener('click', verMas)
